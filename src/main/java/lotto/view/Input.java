28139552af1337994eac64c1a99048bf847f2460
TNALSA <msa1859@naver.com>
2024-02-13 17:04:58 +0900

    feat: countNumber() 추가

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,10 +1,24 @@
 package lotto.view;
 
+import lotto.message.InputMsg;
+
 import java.util.Scanner;
 
 public class Input {
+    int money;
     Scanner sc = new Scanner(System.in);
+
+    //로또 구매 금액 입력받기
     public void inputMoney(){
-        System.out.print("");
+        System.out.println(InputMsg.MONEY);
+        money = sc.nextInt();
     }
+
+    //구매 금액에 따른 로또 갯수 환산하기
+    public int countNumber(int money){
+        int number = money/1000;
+        return number;
+    }
+
+
 }
