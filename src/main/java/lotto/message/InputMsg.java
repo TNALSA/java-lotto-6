99122ead0c1a4200d201cf728ea65ca3fbadcdfe
TNALSA <msa1859@naver.com>
2024-02-13 17:15:45 +0900

    ãrefactor: OutputMsg 클래스 생성

--- src/main/java/lotto/message/InputMsg.java
+++ src/main/java/lotto/message/InputMsg.java
@@ -3,8 +3,7 @@ package lotto.message;
 public enum InputMsg {
     MONEY("구입금액을 입력해 주세요."),
     WIN("당첨 번호를 입력해 주세요."),
-    BONUS("보너스 번호를 입력해 주세요."),
-    WINTOTAL("당첨 통계");
+    BONUS("보너스 번호를 입력해 주세요.");
 
 
     private final String message;
---
+++ src/main/java/lotto/message/OutputMsg.java
@@ -0,0 +1,23 @@
+package lotto.message;
+
+public enum OutputMsg {
+    LOTTO("개를 구매했습니다."),
+    TOTAL("당첨 통계"),
+    LINE("---"),
+    THREE_CORRECT("3개 일치 (5,000원) - "),
+    FOUR_CORRECT("4개 일치 (50,000원) - "),
+    FIVE_CORRECT("5개 일치 (1,500,000원) - "),
+    FIVE_BONUS_CORRECT("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX_CORRECT("6개 일치 (2,000,000,000원) - ");
+
+    private final String message;
+
+    OutputMsg(String msg){
+        this.message = msg;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+}
