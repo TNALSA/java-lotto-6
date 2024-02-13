f9271db0407909349c74216436482189acc49205
TNALSA <msa1859@naver.com>
2024-02-13 13:42:38 +0900

    ãfeat: inputMoney() 추가

---
+++ src/main/java/lotto/view/Input.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import java.util.Scanner;
+
+public class Input {
+    Scanner sc = new Scanner(System.in);
+    public void inputMoney(){
+        System.out.print("");
+    }
+}
