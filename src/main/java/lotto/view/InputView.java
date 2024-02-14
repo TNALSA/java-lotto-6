08e1341cdc6babb6207733c2973f956193c53760
TNALSA <msa1859@naver.com>
2024-02-14 11:20:43 +0900

    refactor: MODIFIED class name AND code

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,13 +1,11 @@
 package lotto.view;
 
 import lotto.message.InputMsg;
-import org.assertj.core.util.Arrays;
 
-import java.awt.*;
 import java.util.List;
 import java.util.Scanner;
 
-public class Input {
+public class InputView {
     //로또 구매 금액
     int money;
 
@@ -24,26 +22,16 @@ public class Input {
     //로또 구매 금액 입력받기
     public void inputMoney(){
         System.out.println(InputMsg.MONEY);
-        money = sc.nextInt();
     }
 
     //로또 당첨번호 입력하기
     public void inputNumber(){
         System.out.println(InputMsg.WIN);
-        winNumber = sc.next();
     }
 
     //보너스 번호 입력받기
     public void inputBonus(){
         System.out.println(InputMsg.BONUS);
-        bonus = sc.nextInt();
     }
-
-    //구매 금액에 따른 로또 갯수 환산하기
-    public int countNumber(int money){
-        int number = money/1000;
-        return number;
-    }
-
 
 }
